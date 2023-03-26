8387939c4de623331597ca67730db6c06494f2cc
Gian1500 <gianuniversidad@gmail.com>
2023-03-26 17:49:33 -0300

    Primmer Commit

---
+++ src/main/java/com/example/apirest/entities/Domicilio.java
@@ -0,0 +1,30 @@
+package com.example.apirest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+
+@Entity
+@Table(name = "domicilio")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Audited
+public class Domicilio extends Base{
+
+    @Column(name = "calle")
+    private String calle;
+
+    @Column(name = "numero")
+    private int numero;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "fk_localidad")
+    private Localidad localidad;
+}
